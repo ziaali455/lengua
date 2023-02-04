19b535a90e2353bc4eda076403480c79ebce0ff9
ziaali455 <76856018+ziaali455@users.noreply.github.com>
2023-02-04 13:35:07 -0500

    Update langlist.py

--- langlist.py
+++ langlist.py
@@ -10,7 +10,7 @@ from gensim.models import KeyedVectors
 #web component
 st.markdown("# Lengua.io")
 st.markdown("_Created by Ali Zia (az2741@columbia.edu) and Chris Tengey (cdt50@georgetown.edu)_")
-st.markdown('This website uses NLP/AI to help language learners find new words. You can read an article about it here.')
+st.markdown('This website uses NLP/AI to help language learners find new words. You can read an article about it here. (coming soon)')
 st.markdown('Looking for words similar to one you already know? **Type the lowercase word in English and press Enter below.**')
 chosenLanguage = st.selectbox(
     'Pick a language: ',
